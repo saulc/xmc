3532cc87df3fe766443f9a49ad89765164ea7df1
Saul <saulc@users.noreply.github.com>
2025-04-27 21:43:12 -0700

    moved
    to files

--- xfiles.py
+++ xfiles.py
@@ -5,6 +5,7 @@ import tkinter as tk
 from tkinter import ttk
 
 import subprocess
+import request
  
 
 
@@ -76,6 +77,127 @@ class xfiles:
         print(p)
         return p
       
+
+    def getDirFiles(self, i, path=None): 
+        mi = []
+        ri = i.copy()
+        #check for directories add items to q
+        for f in i:
+            print('checking item:',f, path)
+            if path==None:
+                p  = self.path+f
+            else: p = path
+            print('checking path:',p)
+            if not os.path.isfile(p):
+                mf, ff = self.getFiles(p)
+                mi.append((p,mf))
+                mi.append((p,ff))
+                ri.remove(f) 
+                # ri.extend(ff)
+                print(mf)
+        print(len(mi), mi)
+        if len(mi) >0:
+
+            for mm in mi:
+                for f in mm[1]:
+                    print('  checking item:',f)
+                    fp = mm[0]+'/'+f
+                    print('  checking item:',fp)
+                    md = []
+                    if not os.path.isfile(str(fp)):
+                        md = self.getDirFiles([f+'/'], fp)
+                        print(md)
+                        mdd = [ k if k.endswith(self.ui.suffixes) else None for k in md]
+                        mz = []
+                        for k in mdd:
+                            print(k)
+                            if k != None:
+                                if k is list:
+                                    mz.extend(k)
+                                else:
+                                    mz.append(k)
+
+                        print('zzz', mz)
+                        ri.extend(mz) 
+                    else:
+                        print('adding item:', fp)
+                        ri.append(fp) 
+        return ri
+
+
+    def addItemsToDb(self, q):
+        #q = (query, filename)
+        nr = []
+        if q == None:
+            i = self.ui.get_selected_items()
+            mi = self.getDirFiles(i) 
+            i.extend(mi)
+
+
+            print('items:', i) 
+
+        else: 
+            #custom query one file at a time only!
+            i = [self.path + q[1]]
+            print('query db', i)
+        # print(i)
+        n = 0
+        for m in i: 
+
+            isvid = m.endswith(self.ui.suffixes) 
+            if  os.path.isfile(m) and isvid: 
+                print(isvid, m)
+                print('checking item', m)
+
+                # self.progressVar.set( 100* n // (len(i)) )
+                # n += 1
+                # self.progressVar.set(self.progress)
+                # self.updateProgress()
+                tv = self.ui.rb.get()==2 
+                if q != None:
+                    rr = request.qdb(q[0], tv)
+                else: rr = request.qdb(m, tv)
+              
+                if rr == None:
+                    nr.append(m)
+                else: 
+                    path = m
+                    print('path  ---:', m)
+                    fs = os.path.getsize(path)
+                    # print(fs, 2**30)
+                    fs /= (2**30)
+                    if self.ui.rb.get() == 1: 
+
+                        duration, dim = self.ui.get_video_metadata(path) #add video info to db
+                        rr.append(str(duration))
+                        rr.append(str(dim[0]))
+                        rr.append(str(dim[1]))
+                        rr.append(str(fs))
+                        print(path, duration, dim)
+                        self.ui.addMovie(rr)
+                    else:  
+
+                        duration, dim = self.ui.get_video_metadata(path) #add video info to db
+                       
+
+                        print(path, duration, dim, fs)
+                        ei = self.ui.request.getTvInfo(rr[1], m)
+                        ei[5] = m[0:m.rfind('/')+1]
+                        ei[6] = m.split('/')[-1]
+
+                        ei.extend([str(duration), str(dim[0]), str(dim[1]), str(fs)])
+                        shows = [ i[0] for i in xdb.getAllTvShows() ]
+                        if rr[0] not in shows:
+                            print(len(rr))
+                            self.ui.xdb.addTv(rr, True)
+
+                        self.ui.addTv(ei)
+                        # self.addTvShow()
+ 
+                    request.getPoster(rr[2], tv) 
+
+        print('no reponse:', len(nr), nr)
+        return nr
 
     def getFiles(self, path): 
         files = os.listdir(path)
